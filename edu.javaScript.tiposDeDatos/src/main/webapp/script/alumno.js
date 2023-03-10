ff90be2f668c4ca4bab737278534bedb54ee2d3e
DESKTOP-USHSCCC\csi-r <csi22-rcoriaz@colegioaltair.net>
2023-03-10 10:10:29 +0100

    no message

--- edu.javaScript.tiposDeDatos/src/main/webapp/script/alumno.js
+++ edu.javaScript.tiposDeDatos/src/main/webapp/script/alumno.js
@@ -1,13 +1,16 @@
 function ejemplo() {
 
-	function alumno(nombre, apellidos, edad) {
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.edad = edad;
+	class alumno {
+		constructor(nombre, apellidos, edad) {
+			this.nombre = nombre;
+			this.apellidos = apellidos;
+			this.edad = edad;
+		}
 	}
 
 	var raul = new alumno("Raúl", "Cordero Diaz", 18);
-
+	
+	document.writeln(typeof(raul));
 	document.writeln(raul instanceof alumno);
 	document.writeln(raul instanceof Object);
 
